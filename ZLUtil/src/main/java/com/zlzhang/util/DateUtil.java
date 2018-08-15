dedf937f26d376bfa268060d2fe580c819766e8a
zhangzl <zhangzl@gdu-tech.com>
2018-08-15 09:16:09 +0800

    添加时间操作相关方法

--- ZLUtil/src/main/java/com/zlzhang/util/DateUtil.java
+++ ZLUtil/src/main/java/com/zlzhang/util/DateUtil.java
@@ -2,6 +2,7 @@ package com.zlzhang.util;
 
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -14,6 +15,7 @@ public class DateUtil {
 
     /**
      * 获取当前时区
+     *
      * @return
      */
     public static String getTimeZone() {
@@ -33,5 +35,49 @@ public class DateUtil {
         @SuppressWarnings("unused")//  编译过程中，忽略这种类型的警告
                 String times = sdr.format(new Date(timestamp * 1000L));
         return times;
+    }
+
+    /**
+     * 获取当前时间的string类型
+     *
+     * @return
+     */
+    public static String getCurrentDateString(String format) {
+        Date date = new Date();
+        return getDateString(date, format);
+    }
+
+    /**
+     * 根据Date和类型获取时间String
+     *
+     * @param date
+     * @param format
+     * @return
+     */
+    public static String getDateString(Date date, String format) {
+        SimpleDateFormat sdfd = new SimpleDateFormat(format);
+        return sdfd.format(date);
+    }
+
+    /**
+     * 获取当前时间之前多少天的时间
+     *
+     * @param currentDate
+     * @param days
+     * @return
+     */
+    public static Date getBeforeDate(Date currentDate, int days) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        int i = 0;
+        while (i < days) {
+            calendar.add(Calendar.DATE, 1);
+            i++;
+            if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY ||
+                    calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+                i--;
+            }
+        }
+        return calendar.getTime();
     }
 }
